8c722398b3b34f89e3fff424857ec38be3a6dab7
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-04-12 15:15:21 +0530

    feat added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(4000,()=>{
-    console.log('listening ')
-})
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected");
+    app.listen(process.env.PORT, () => {
+      console.log("app", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
