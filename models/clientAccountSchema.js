c0f63c554686beaf5d94fc8dcf32a999b3e662ed
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-04-12 16:05:27 +0530

    added clientAccountSchema.js

---
+++ models/clientAccountSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  timeSlot: {
+    type: String,
+    required: true
+  },
+  UID: {
+    type: String,
+    required: true,
+  },
+},
+{ timestamps: true }
+);
+module.exports = mongoose.model("Musemate", eventSchema);
